36f4985aae63960df7085ab9385c8881b1b9ace7
levy <897895407@qq.com>
2024-06-09 08:57:57 +0800

    feat: fastapi

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ tools = [current_date]
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "你是个智能助手，擅长利用工具解决问题。当用户提问时，如果知道利用工具获取答案后，严谨地回答问题即可，不要反问"),
-    ("placeholder", "{chat_history}"),
+    # ("placeholder", "{chat_history}"),
     ("human", "{input}"),
     ("placeholder", "{agent_scratchpad}"),
 ])
@@ -33,6 +33,27 @@ prompt = ChatPromptTemplate.from_messages([
 agent = create_tool_calling_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-print(
-    agent_executor.invoke({"input": "今天几号?"})
+
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Law API Server",
+    version="1.0",
+    description="glm law competition app created by levy",
 )
+
+
+from pydantic import BaseModel
+class Query(BaseModel):
+    q: str
+
+# 使用 post, 请求时没有 encode 的需求, 用 get 则需要客户端　encode，否则中文无法识别（curl里是这样)
+@app.post("/chat")
+def chat(query: Query):
+    return agent_executor.invoke({"input": query.q})
+
+if __name__ == "__main__":
+    import uvicorn
+
+    #curl "http://localhost:9000/chat" -X POST -H "Content-Type: application/json" -d "{\"q\":\"问题\"}"
+    uvicorn.run(app, host="localhost", port=9000)
